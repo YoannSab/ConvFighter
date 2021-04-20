9cd81cad33dc4981365815039fd082033c3c4f7d
Yoaaaaaaaann <82182246+Yoaaaaaaaann@users.noreply.github.com>
2021-04-21 00:29:12 +0200

    tesdt

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame as pg
 from player import Player
 from projectile import Projectile
+import time
+from threading import Timer
+from timer import My_Timer
 
 
 # Création de la classe Game
@@ -8,18 +11,204 @@ class Game:
 
     def __init__(self):
 
-        self.proj1 = Projectile('balle de fusil',10,'assets/bullet.png')
-        self.proj2 = Projectile('balle de fusil2', 10, 'assets/bullet2.png')
-        self.P1 = Player(1,'Marc', 150, 1, 'assets/004-evil.png', 10, 5, self.proj1)
-        self.P1.pos_start(1)
-        self.P2 = Player(2, 'Yoann', 110, 1, 'assets/002-angel.png', 7,2, self.proj2)
-        self.P2.pos_start(2)
+        self.proj1 = Projectile('balle de fusil', 10, 'assets/fire_ball.png', 30)
+        self.proj2 = Projectile('balle de fusil2', 10, 'assets/arrow.png',30)
+        self.marc = Player('Marc', 150, 1, 'assets/004-evil.png','assets/marc.png', 10, 5, self.proj1,100, 10)
+        self.yoann = Player('Yoann', 110, 1, 'assets/002-angel.png','assets/yoann.png', 7, 2, self.proj2,100, 10)
+        self.tristan = Player('Tristan', 160, 1, 'assets/001-sleeping.png','assets/tristan.png', 18, 6, self.proj1,100,10)
+        self.arthur = Player('Arthur', 100, 1, 'assets/006-centaur.png','assets/arthur.png',8, 6, self.proj1,100,10)
+        self.nathan = Player('Nathan', 120, 1, 'assets/003-bandit.png','assets/nathan.png', 10, 6, self.proj2,100,10)
+        self.pierre = Player('Pierre', 80, 1, 'assets/005-nymph butterfly.png','assets/pierre.png', 6, 40, self.proj2,100,10)
+        self.list_player = [self.marc, self.yoann, self.tristan, self.arthur, self.nathan, self.pierre]
+        self.P1 = None
+        self.P2 = None
         self.pressed = {}
         self.projs1 = []
         self.projs2 = []
+        self.is_playing = False
+        self.img_go = pg.image.load("assets/game_over.png")
+        self.img_go = pg.transform.scale(self.img_go, (300, 200))
+        self.img_go_rect = self.img_go.get_rect()
+        self.img_go_rect.x = 400
+        self.img_go_rect.y = 100
+        self.img_replay = pg.image.load("assets/replay.png")
+        self.img_replay = pg.transform.scale(self.img_replay, (300, 80))
+        self.img_replay_rect = self.img_replay.get_rect()
+        self.img_replay_rect.x = 400
+        self.img_replay_rect.y = 600
+        self.game_over = False
+        self.winner = None
+        # regen du mana toutes les 1 secondes
+        self.timer = My_Timer(1.0, self.mana_regen_in_game)
 
     def create_proj(self, player):
-        if player.number_play==1:
-            self.projs1.append(Projectile(player.projectile.name, player.projectile.damage, player.projectile.image_string))
+        if player.number_play == 1:
+            self.projs1.append(
+                Projectile(player.projectile.name, player.projectile.damage, player.projectile.image_string,player.projectile.mana_cost))
         else:
-            self.projs2.append(Projectile(player.projectile.name, player.projectile.damage, player.projectile.image_string))
+            self.projs2.append(
+                Projectile(player.projectile.name, player.projectile.damage, player.projectile.image_string, player.projectile.mana_cost))
+
+    def choose_player(self, player):
+        if self.P1 is None:
+            self.P1 = player
+            self.P1.number_play = 1
+        else:
+            self.P2 = player
+            self.P2.number_play = 2
+        self.launch_game()
+
+    def launch_game(self):
+        if self.P1 is not None and self.P2 is not None:
+            self.P1.last_direction = 'Right'
+            self.P1.pos_start()
+            self.P2.last_direction = 'Left'
+            self.P2.pos_start()
+            self.is_playing = True
+            self.timer.start()
+
+    def try_game_over(self):
+        if self.is_playing:
+            if self.P1.health <= 0 or self.P2.health <= 0:
+                self.game_over = True
+                self.is_playing = False
+                self.timer.stop()
+                if self.P1.health <= 0:
+                    self.winner = self.P2
+                else:
+                    self.winner = self.P1
+
+    def new_game(self):
+        for player in self.list_player:
+            player.health = player.max_health
+            player.mana = player.mana_max
+        self.P1 = None
+        self.P2 = None
+        self.winner = None
+        self.projs1 = []
+        self.projs2 = []
+        self.game_over = False
+
+    def end_window(self, screen):
+        screen.blit(self.img_go, self.img_go_rect)
+        screen.blit(self.img_replay, self.img_replay_rect)
+        text_nom_winner = pg.font.SysFont("Calibri", 60).render("Le gagnant est "+self.winner.name+", Bravo !", 1, (0, 187, 254))
+        screen.blit(text_nom_winner, (200, 400))
+
+    def choice_window(self, screen, police):
+        self.marc.splashart_rect.x = 300
+        self.marc.splashart_rect.y = 100
+        screen.blit(self.marc.splashart, self.marc.splashart_rect)
+        text_nom_marc = police.render(str(self.marc.name), 1, (0, 187, 254))
+        screen.blit(text_nom_marc, (self.marc.splashart_rect.x + 30, self.marc.splashart_rect.y - 40))
+
+        self.yoann.splashart_rect.x = 500
+        self.yoann.splashart_rect.y = 100
+        screen.blit(self.yoann.splashart, self.yoann.splashart_rect)
+        text_nom_yoann = police.render(str(self.yoann.name), 1, (0, 187, 254))
+        screen.blit(text_nom_yoann, (self.yoann.splashart_rect.x + 30, self.yoann.splashart_rect.y - 40))
+
+        self.tristan.splashart_rect.x = 700
+        self.tristan.splashart_rect.y = 100
+        screen.blit(self.tristan.splashart, self.tristan.splashart_rect)
+        text_nom_tristan = police.render(str(self.tristan.name), 1, (0, 187, 254))
+        screen.blit(text_nom_tristan, (self.tristan.splashart_rect.x + 30, self.tristan.splashart_rect.y - 40))
+
+        self.arthur.splashart_rect.x = 300
+        self.arthur.splashart_rect.y = 350
+        screen.blit(self.arthur.splashart, self.arthur.splashart_rect)
+        text_nom_arthur = police.render(str(self.arthur.name), 1, (0, 187, 254))
+        screen.blit(text_nom_arthur, (self.arthur.splashart_rect.x + 30, self.arthur.splashart_rect.y - 40))
+
+        self.nathan.splashart_rect.x = 500
+        self.nathan.splashart_rect.y = 350
+        screen.blit(self.nathan.splashart, self.nathan.splashart_rect)
+        text_nom_nathan = police.render(str(self.nathan.name), 1, (0, 187, 254))
+        screen.blit(text_nom_nathan, (self.nathan.splashart_rect.x + 30, self.nathan.splashart_rect.y - 40))
+
+        self.pierre.splashart_rect.x = 700
+        self.pierre.splashart_rect.y = 350
+        screen.blit(self.pierre.splashart, self.pierre.splashart_rect)
+        text_nom_pierre = police.render(str(self.pierre.name), 1, (0, 187, 254))
+        screen.blit(text_nom_pierre, (self.pierre.splashart_rect.x + 30, self.pierre.splashart_rect.y - 40))
+
+    def window_update(self, screen):
+        # Appliquer image P1 et P2
+        screen.blit(self.P1.image, self.P1.rect)
+        screen.blit(self.P2.image, self.P2.rect)
+
+        # affichage barre d'hp P1
+        bar_hpP1 = pg.Surface((self.P1.rect.width, 10))
+        rect_hpP1 = pg.Rect((3, 2), (round((self.P1.health / self.P1.max_health) * (self.P1.rect.width - 6)), 5))
+        bar_hpP1.fill(pg.Color(0, 69, 11))
+        bar_hpP1.fill(pg.Color(3, 233, 42), rect_hpP1)
+        screen.blit(bar_hpP1, (self.P1.rect.x, self.P1.rect.y - 30))
+
+        # affichage barre d'hp P2
+        bar_hpP2 = pg.Surface((self.P2.rect.width, 10))
+        rect_hpP2 = pg.Rect((3, 2), (round((self.P2.health / self.P2.max_health) * (self.P2.rect.width - 6)), 5))
+        bar_hpP2.fill(pg.Color(0, 69, 11))
+        bar_hpP2.fill(pg.Color(3, 233, 42), rect_hpP2)
+        screen.blit(bar_hpP2, (self.P2.rect.x, self.P2.rect.y - 30))
+
+        # affichage barre de mana P1
+        bar_manaP1 = pg.Surface((self.P1.rect.width, 10))
+        rect_manaP1 = pg.Rect((3, 2), (round((self.P1.mana / self.P1.mana_max) * (self.P1.rect.width - 6)), 5))
+        bar_manaP1.fill(pg.Color(0, 121, 254))
+        bar_manaP1.fill(pg.Color(0, 254, 204), rect_manaP1)
+        screen.blit(bar_manaP1, (self.P1.rect.x, self.P1.rect.y - 15))
+
+        # affichage barre de mana P2
+        bar_manaP2 = pg.Surface((self.P2.rect.width, 10))
+        rect_manaP2 = pg.Rect((3, 2), (round((self.P2.mana / self.P2.mana_max) * (self.P2.rect.width - 6)), 5))
+        bar_manaP2.fill(pg.Color(0, 121, 254))
+        bar_manaP2.fill(pg.Color(0, 254, 204), rect_manaP2)
+        screen.blit(bar_manaP2, (self.P2.rect.x, self.P2.rect.y - 15))
+
+        # Verifier les touches pressées
+        # P1
+        self.P1.try_jump()
+        if self.pressed.get(pg.K_d) and self.P1.rect.x < 1050:  # and not self.P1.distance_cac(self.P2):
+            time.sleep(0.001)
+            self.P1.move_right()
+            if self.P1.jump:
+                self.P1.jump_direction = 'Right'
+        if self.pressed.get(pg.K_q) and self.P1.rect.x > -100:  # and not self.P1.distance_cac(self.P2):
+            time.sleep(0.001)
+            self.P1.move_left()
+            if self.P1.jump:
+                self.P1.jump_direction = 'Left'
+
+        # P2
+        self.P2.try_jump()
+        if self.pressed.get(pg.K_RIGHT) and self.P2.rect.x < 1050:
+            time.sleep(0.001)
+            self.P2.move_right()
+            if self.P2.jump:
+                self.P2.jump_direction = 'Right'
+        if self.pressed.get(pg.K_LEFT) and self.P2.rect.x > -100:
+            time.sleep(0.001)
+            self.P2.move_left()
+            if self.P2.jump:
+                self.P2.jump_direction = 'Left'
+
+        # Appliquer les projectiles / séparation des projectiles de P1 et P2
+        for proj in self.projs1:
+            if proj.shot:
+                screen.blit(proj.image, proj.rect)
+                proj.coord_update()
+                proj.try_damage(self.P2)
+
+        for proj in self.projs2:
+            if proj.shot:
+                screen.blit(proj.image, proj.rect)
+                proj.coord_update()
+                proj.try_damage(self.P1)
+
+    def mana_regen_in_game(self):
+        self.P1.regen_mana()
+        self.P2.regen_mana()
+
+
+
+
--- player.py
+++ player.py
@@ -1,37 +1,40 @@
 import pygame as pg
 import time
 
+
 # Création de la classe joueur
 class Player(pg.sprite.Sprite):
 
-    def __init__(self, number_play,name,max_health, velocity,image, attack, heal, projectile):
+    def __init__(self, name, max_health, velocity, image_string, splashart_string, attack, heal, projectile, mana_max, mana_regen):
         super().__init__()
         self.name = name
-        self.number_play = number_play
         self.health = max_health
         self.max_health = max_health
         self.velocity = velocity
         self.attack = attack
         self.heal = heal
+        self.mana = mana_max
+        self.mana_max = mana_max
+        self.mana_regen = mana_regen
         self.projectile = projectile
-        self.image_string = image
+        self.image_string = image_string
         self.image = pg.image.load(self.image_string)
-        self.image = pg.transform.scale(self.image, (140, 140))
+        self.image = pg.transform.scale(self.image, (70, 70))
+        self.splashart = pg.transform.scale(pg.image.load(splashart_string), (140, 140))
+        self.splashart_rect = self.splashart.get_rect()
         self.rect = self.image.get_rect()
-        self.rect.y = 520
-        if self.number_play==1:
-            self.last_direction = 'Right'   #met en mémoire la dernière direction
-        if self.number_play ==2:
-            self.last_direction = 'Left'
-        self.jump = False #est ce que le joueur veut sauter
-        self.reach_top = False #est ce que le joueur a atteint en haut
+        self.jump = False  # est ce que le joueur veut sauter
+        self.reach_top = False  # est ce que le joueur a atteint en haut
         self.jump_direction = None
+        self.number_play = None
+        self.last_direction = None
 
-    def pos_start(self, number_play):
-        if number_play == 1:
+    def pos_start(self):
+        if self.number_play == 1:
             self.rect.x = 200
-        elif number_play == 2:
+        elif self.number_play == 2:
             self.rect.x = 800
+        self.rect.y = 520
 
     def move_right(self):
         self.rect.x += self.velocity
@@ -42,13 +45,13 @@ class Player(pg.sprite.Sprite):
         self.last_direction = 'Left'
 
     def move_up(self):
-        self.rect.y -= (self.velocity+2)
+        self.rect.y -= (self.velocity + 2)
 
     def move_down(self):
-        self.rect.y += (self.velocity+2)
+        self.rect.y += (self.velocity + 2)
 
     def distance_cac(self, other_player):
-        if abs(other_player.rect.x - self.rect.x) < 100 and abs(other_player.rect.y - self.rect.y) < 60:
+        if self.rect.colliderect(other_player.rect):
             return True
         else:
             return False
@@ -56,42 +59,52 @@ class Player(pg.sprite.Sprite):
     def set_attacked(self, damage):
         self.health -= damage
 
-    def set_healed(self, heal):
-        self.health += heal
+    def get_healed(self):
+        self.mana -= 20
+        if self.health >= self.max_health - self.heal:
+            self.health = self.max_health
+        else:
+            self.health += self.heal
 
     def punch(self, other_player):
         if self.distance_cac(other_player):
             other_player.set_attacked(self.attack)
+            self.mana -= 5
+
+    def regen_mana(self):
+        if self.mana <= self.mana_max - self.mana_regen:
+            self.mana += self.mana_regen
+        else:
+            self.mana = self.mana_max
 
     def is_up(self):
         return self.rect.y != 520
 
-    def try_jump(self): #essaie tout le temps le saut mais le fait ssi jump est true
+    def try_jump(self):  # essaie tout le temps le saut mais le fait ssi jump est true
         if self.jump:
-            if self.rect.y>300 and not self.reach_top: #s'il n'a pas atteint deja atteint le top et quil est en dessous du max, on fait monter
-                if self.jump_direction ==None:
+            if self.rect.y > 300 and not self.reach_top:  # s'il n'a pas atteint deja atteint le top et quil est en dessous du max, on fait monter
+                if self.jump_direction is None:
                     self.move_up()
-                if self.jump_direction== 'Right':
+                if self.jump_direction == 'Right':
                     self.move_up()
                     self.move_right()
-                if self.jump_direction== 'Left':
+                if self.jump_direction == 'Left':
                     self.move_up()
                     self.move_left()
             else:
                 self.reach_top = True
-                if self.jump_direction==None:
-                    self.move_down()  #sinon, on faut descendre et on dit quil a deja atteint le top
+                if self.jump_direction is None:
+                    self.move_down()  # sinon, on faut descendre et on dit quil a deja atteint le top
                 if self.jump_direction == 'Right':
                     self.move_down()
                     self.move_right()
-                if self.jump_direction =='Left':
+                if self.jump_direction == 'Left':
                     self.move_down()
                     self.move_left()
-                if not self.is_up(): #s'il est de nouveau a terre, jump devient false
+                if not self.is_up():  # s'il est de nouveau a terre, jump devient false
                     self.jump = False
 
     def init_jump(self):
         self.reach_top = False
         self.jump_direction = None
         self.jump = True
-
